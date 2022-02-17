5f469aa6e6249185204941e220491f3a290b9744
Manuel Riviere <manuel@ioversal.com>
2022-02-17 15:09:53 +0100

    added commands and removed some redundancies

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var tcp = require('../../tcp');
 var instance_skel = require('../../instance_skel');
+const { isNull } = require('lodash');
 var debug;
 var log;
 
@@ -125,82 +126,58 @@ instance.prototype.destroy = function () {
 
 instance.prototype.actions = function (system) {
 	var self = this;
+	var playbackOptions = { type: 'number', label: 'Playback Id', id: 'id', default: '1', };
+	var scriptOptions = { type: 'textinput', label: 'Script', id: 'script', default: '',}
+	var cueOptions = { type: 'number', label: 'Cue Id', id: 'cueId', default: '1', };
+	var preloadTimeOptions = { type: 'number', label: 'Preload Time', id: 'preloadTime', default: '1', };
+	var fadeTimeOptions = { type: 'number', label: 'Fade Time', id: 'fadeTime', default: '1', };
+	var targetOptions = { type: 'textinput', label: 'Target', id: 'target', default: 'Playback1' };
+	var intervalOptions = { type: 'number', label: 'Interval ms', id: 'interval', default: 100 };
 	self.system.emit('instance_actions', self.id, {
 		'script': {
 			label: 'Run Script',
-			options: [{
-				type: 'textinput',
-				label: 'Script',
-				id: 'script',
-				default: ''
-			}]
+			options: [playbackOptions, scriptOptions]
 		},
 		'play': {
 			label: 'Play',
-			options: [{
-				type: 'number',
-				label: 'Playback Id',
-				id: 'id',
-				default: '1',
-			}]
-		},
-		'stop': {
-			label: 'Stop',
-			options: [{
-				type: 'number',
-				label: 'Playback Id',
-				id: 'id',
-				default: '1',
-			}]
+			options: [playbackOptions]
 		},
 		'pause': {
 			label: 'Pause',
-			options: [{
-				type: 'textinput',
-				label: 'Playback Id',
-				id: 'id',
-				default: '1',
-				regex: self.REGEX_NUMBER
-			}]
+			options: [playbackOptions]
 		},
+		'stop': {
+			label: 'Stop',
+			options: [playbackOptions]
+		},
+		'goToCue': {
+			label: 'Go to Cue',
+			options: [playbackOptions, cueOptions,]
+		},
+		'fadeToCue': {
+			label: 'Fade to Cue',
+			options: [playbackOptions, cueOptions, preloadTimeOptions, fadeTimeOptions,]
+		},
+		'gotoNextCue': {
+			label: 'Go to Next Cue',
+			options: [playbackOptions,]
+		},
+		'gotoPreviousCue': {
+			label: 'Go to Previous Cue',
+			options: [playbackOptions,]
+		},
+
 		'poll': {
 			label: 'Poll',
-			options: [
-				{
-					type: 'textinput',
-					label: 'Target',
-					id: 'target',
-					default: 'Playback1'
-				},
-			]
+			options: [targetOptions,]
 		},
 		'update_polling_target': {
 			label: 'Update polling target',
-			options: [
-				{
-					type: 'textinput',
-					label: 'Target',
-					id: 'target',
-					default: 'Playback1'
-				},
-			]
+			options: [targetOptions,]
 		},
 		'start_timer': {
 			label: 'Start Polling Timer',
-			options: [
-				{
-					type: 'textinput',
-					label: 'Target',
-					id: 'target',
-					default: 'Playback1'
-				},
-				{
-					type: 'number',
-					label: 'Interval ms',
-					id: 'interval',
-					default: 100
-				},
-			]
+			options: [targetOptions, intervalOptions,]
 		},
 		'stop_timer': {
 			label: 'Stop Polling Timer',
@@ -210,21 +187,35 @@ instance.prototype.actions = function (system) {
 
 instance.prototype.action = function (action) {
 	var self = this;
+	var playback = 'Playback' + action.options.id;
+	var cueId = action.options.cueId;
+
 	debug('run vertex action:', action);
 	var cmd;
-
 	switch (action.action) {
 		case 'script':
 			cmd = action.options.script;
 			break;
 		case 'play':
-			cmd = 'Playback' + action.options.id + '.Play';
+			cmd = self.buildCommand(playback, 'Play');
 			break;
 		case 'stop':
-			cmd = 'Playback' + action.options.id + '.Stop';
+			cmd = self.buildCommand(playback, 'Stop');
 			break;
 		case 'pause':
-			cmd = 'Playback' + action.options.id + '.Pause';
+			cmd = self.buildCommand(playback, 'Pause');
+			break;
+		case 'goToCue':
+			cmd = self.buildCommand(playback, 'GotoCue', cueId);
+			break;
+		case 'fadeToCue':
+			cmd = self.buildCommand(playback, 'FadeToCue', [cueId, action.options.preloadTime, action.options.fadeTime]);
+			break;
+		case 'gotoNextCue':
+			cmd = self.buildCommand(playback, 'GotoNextCue');
+			break;
+		case 'gotoPreviousCue':
+			cmd = self.buildCommand(playback, 'GotoPrevCue');
 			break;
 		case 'poll':
 			self.updatePollingTarget(action.options.target);
@@ -250,6 +241,17 @@ instance.prototype.action = function (action) {
 		}
 	}
 };
+
+instance.prototype.buildCommand = function (target, command, arguments) {
+	var self = this;
+	var result = target + '.' + command;
+	if (Array.isArray(arguments)) {
+		result += ' ' + arguments.join(',');
+	} else if (arguments != null) {
+		result += ' ' + arguments.toString();
+	}
+	return result;
+}
 
 instance.prototype.updatePollingTarget = function (target) {
 	var self = this;
