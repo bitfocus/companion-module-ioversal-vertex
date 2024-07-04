ac07604c1a4d256f210be624abb923c122d9a88e
VideoBSO <video@staatsoper.de>
2024-07-04 16:50:13 +0200

    new entrypoint `vertex.js`

---
+++ vertex.js
@@ -0,0 +1,78 @@
+const { InstanceBase, Regex, runEntrypoint } = require('@companion-module/base')
+const UpgradeScripts = require('./upgrades')
+
+class vertexInstance extends InstanceBase {
+	constructor(internal) {
+		super(internal)
+	}
+
+	async init(config) {
+		this.config = config
+
+		this.updateStatus('ok')
+
+		this.updateActions() // export Actions
+	}
+
+	async destroy() {
+		this.log('debug', 'destroy')
+	}
+
+	async configUpdated(config) {
+		this.config = config
+	}
+
+	// Return config fields for web config
+	getConfigFields() {
+		return [
+				{
+					type: 'textinput',
+					id: 'host',
+					label: 'Vertex Instance IP',
+					width: 6,
+					regex: Regex.IP
+				}
+		]
+	}
+
+	updateActions() {
+		
+	}
+
+	init_tcp() {
+		var self = this;
+
+		if (self.socket !== undefined) {
+			self.socket.destroy();
+			delete self.socket;
+		}
+
+		if (self.config.host) {
+			var port = 50009;
+
+			self.socket = new tcp(self.config.host, port);
+
+			self.socket.on('status_change', function (status, message) {
+				self.status(status, message);
+			});
+
+			self.socket.on('error', function (err) {
+				debug("Network error", err);
+				self.log('error', "Network error: " + err.message);
+			});
+
+			self.socket.on('connect', function () {
+				debug("Connected");
+				if (self.config.type === 'disp') {
+					self.socket.send('authenticate 1\r\n');
+				}
+			});
+
+			self.socket.on('data', function (data) {
+				self.processIncomingData(data);
+			});
+		}
+	}
+}
+
+runEntrypoint(vertexInstance, UpgradeScripts)
