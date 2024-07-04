2ea3753f212329a478b12d1cb6b355c18139e5ff
VideoBSO <video@staatsoper.de>
2024-07-04 18:56:08 +0200

    add all functions and implement actions

--- vertex.js
+++ vertex.js
@@ -38,8 +38,155 @@ class vertexInstance extends InstanceBase {
 		]
 	}
 
+	buildCommand (target, command, args) {
+			var self = this;
+			var result = target + '.' + command;
+			if (Array.isArray(arguments)) {
+				result += ' ' + arguments.join(',');
+			} else if (arguments != null) {
+				result += ' ' + arguments.toString();
+			}
+			return result;
+	}
+
 	updateActions() {
+		const actions = {}
+		var self = this
+
+		var playbackOptions = { type: 'number', label: 'Playback Id', id: 'id', default: '1', };
+		var scriptOptions = { type: 'textinput', label: 'Script', id: 'script', default: '',}
+		var cueOptions = { type: 'number', label: 'Cue Id', id: 'cueId', default: '1', };
+		var preloadTimeOptions = { type: 'number', label: 'Preload Time', id: 'preloadTime', default: '1', };
+		var fadeTimeOptions = { type: 'number', label: 'Fade Time', id: 'fadeTime', default: '1', };
+		var targetOptions = { type: 'textinput', label: 'Target', id: 'target', default: 'Playback1' };
+		var intervalOptions = { type: 'number', label: 'Interval ms', id: 'interval', default: 100 };
+
+		var playback = 'Playback' + action.options.id
+		var cueId = actions.options.cueId
+
+		actions['script'] = {
+			name: 'Run Script',
+			options: [playbackOptions, scriptOptions],
+			callback: (action) => {
+				cmd = action.options.script
+			}
+		}
 		
+		actions['play'] = {
+			name: 'Play',
+			options: [playbackOptions],
+			callback: (action) => {
+				cmd = self.buildCommand(playback, 'Play')
+			}
+		}
+		actions['pause'] = {
+			name: 'Pause',
+			options: [playbackOptions],
+			callback: (action) => {
+				cmd = self.buildCommand(playback, 'Pause')
+			}
+		}
+		actions['stop'] = {
+			name: 'Stop',
+			options: [playbackOptions],
+			callback: (action) => {
+				cmd = self.buildCommand(playback, 'Stop')
+			}
+		}
+		actions['goToCue'] = {
+			name: 'Go to Cue',
+			options: [playbackOptions, cueOptions],
+			callback: (action) => {
+				cmd = self.buildCommand(playback, 'GotoCue', cueId)
+			}
+		}
+		actions['fadeToCue'] = {
+			name: 'Fade to Cue',
+			options: [playbackOptions, cueOptions, preloadTimeOptions, fadeTimeOptions],
+			callback: (action) => {
+				cmd = self.buildCommand(playback, 'FadeToCue', [cueId, action.options.preloadTime, action.options.fadeTime])
+				
+			}
+		}
+		actions['gotoNextCue'] = {
+			name: 'Go to Next Cue',
+			options: [playbackOptions],
+			callback: (action) => {
+				cmd = self.buildCommand(playback, 'GotoNextCue')
+			}
+		}
+		actions['gotoPreviousCue'] = {
+			name: 'Go to Previous Cue',
+			options: [playbackOptions],
+			callback: (action) => {
+				cmd = self.buildCommand(playback, 'GotoPreviousCue')
+			}
+		}
+		actions['poll'] = {
+			name: 'Poll',
+			options: [targetOptions],
+			callback: (action) => {
+				self.updatePollingTarget(action.options.target);
+				cmd = 'return CompanionRequest ' + self.pollingTarget;
+			}
+		}
+		actions['update_polling_target'] = {
+			name: 'Update polling target',
+			options: [targetOptions],
+			callback: (action) => {
+				self.updatePollingTarget(action.options.target);
+			}
+		}
+		actions['start_timer'] = {
+			name: 'Start Polling Timer',
+			options: [targetOptions, intervalOptions],
+			callback: (action) => {
+				self.updatePollingTarget(action.options.target);
+				self.startPollingTimer(action.options.interval);
+			}
+		}
+		actions['stop_timer'] = {
+			name: 'Stop Polling Timer',
+			options: [],
+			callback: (action) => {
+					self.stoPollingTimer()
+				}
+		}
+
+		if (cmd !== undefined) {
+
+			if (self.ensureConnection()) {
+				self.log('debug', "sending " + cmd + " to " + self.config.host);
+				self.socket.send(cmd + "\r\n");
+			}
+		}
+	}
+	
+	updatePollingTarg(target) {
+		var self = this;
+		self.pollingTarget = target;
+		self.setVariable('polling_target', target);
+	}
+
+	poll = function () {
+		var self = this;
+		self.socket.send('return CompanionRequest ' + self.pollingTarget + "\r\n");
+	}
+
+	startPollingTimer(interval) {
+		var self = this;
+		clearInterval(self.pollingTimer);
+		self.pollingTimer = setInterval(function () {
+
+			self.poll();
+
+		}, interval);
+
+	}
+
+	stopPollingTimer(interval) {
+		var self = this;
+		clearInterval(self.pollingTimer);
 	}
 
 	init_tcp() {
@@ -75,6 +222,68 @@ class vertexInstance extends InstanceBase {
 			});
 		}
 	}
+	
+	ensureConnection() {
+		self = this;
+		if (self.socket === undefined) {
+			self.init_tcp();
+		}
+		if (self.socket !== undefined && self.socket.connected) {
+			return true;
+		} else {
+			self.log('warning', 'Socket not connected');
+			return false;
+		}
+	}
+
+	processIncomingData(data) {
+		var self = this;
+		self.log('debug', "Data received: " + data);
+		try {
+			var parsed = JSON.parse(data);
+		} catch (error) {
+			self.log('info', 'Unable to parse incoming data as JSON: ' + data);
+			return;
+		}
+		var targets = self.pollingTarget.split(',').map(s => s.trim()).filter(function (e) { return e.trim().length > 0; });
+		targets.forEach(element => {
+			self.setTargetVariables(element, parsed);
+		});
+	}
+
+	setTargetVariables (target, data) {
+		let self = this;
+		let targetData = data[target];
+		if (targetData == null) return;
+		let targetType = targetData['Type'];
+		let targetVariableNames = self.variableNames[targetType];
+		if (targetVariableNames == null) return;
+		targetVariableNames.forEach(varName => {
+			self.setVariable(self.buildVarName(targetType, varName), targetData[varName]);
+		});
+	}
+
+	initVariable() {
+		var self = this;
+		log('debug', "initializing variables");
+		var variables = [
+			{
+				label: 'Polling Target',
+				name: 'polling_target'
+			}
+		];
+
+		for (type in self.variableNames) {
+			self.variableNames[type].forEach(value => {
+				variables.push({ label: value + " of selected " + type, name: self.buildVarName(type, value) });
+			});
+		}
+
+		self.setVariableDefinitions(variables);
+	};
+
+	buildVarName(type, name) { return type + '_' + name; };
+
 }
 
 runEntrypoint(vertexInstance, UpgradeScripts)
